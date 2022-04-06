05a66a28693bba9b67edcf803cfe360d79b7980e
layout-IT <77637040+layout-IT@users.noreply.github.com>
2022-04-06 09:38:12 +0300

    add projects

--- src/Main/Portfolios-page/Portfolios.tsx
+++ src/Main/Portfolios-page/Portfolios.tsx
@@ -5,10 +5,12 @@ import Frispes from '../../assets/images/Frispes.png'
 import Nasscorp from '../../assets/images/Nasscorp.png'
 import SocialNetwork from '../../assets/images/social_network.png'
 import Todolist from '../../assets/images/Todolist.png'
+import TestTask from '../../assets/images/TesTask.png'
+import Calculator from '../../assets/images/Calculator.png'
+import GSIron from '../../assets/images/GsIron.png'
+import TableOnPureJs from '../../assets/images/TableOnPureJs.png'
+import Counter from '../../assets/images/Counter.png'
 import style from './Portfolios.module.scss'
-import photo from '../../assets/images/1.jpg'
-import photo2 from '../../assets/images/2.jpg'
-import photo3 from '../../assets/images/3.jpg'
 import s from '../About-page/About.module.scss'
 
 export function Portfolios() {
@@ -31,11 +33,25 @@ export function Portfolios() {
                     <div className={style.portfolio_items}>
                         <div className={style.portfolio_colums}>
                             <div className={style.portfolio_item}>
+                                <a className={style.portfolioItem_img} href={'https://layout-it.github.io/TestTask/'}
+                                   target={'_blank'}>
+                                    <img src={TestTask} alt="photo"/>
+                                </a>
+                                <div className={style.portfolioItem_title}>React, Redux, TypeScript, MUI, Css-modules, Scss,
+                                    React-dom, React-router-dom, React-redux, Thunk, Formik, Moment, Axios</div>
+                                <div className={style.portfolioItem_subtitle}><a className={style.code}
+                                                                                 href="https://github.com/layout-IT/TestTask">Code</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className={style.portfolio_colums}>
+                            <div className={style.portfolio_item}>
                                 <a className={style.portfolioItem_img} href={'https://layout-it.github.io/ToDolist/'}
                                    target={'_blank'}>
                                     <img src={Todolist} alt="photo"/>
                                 </a>
-                                <div className={style.portfolioItem_title}>ToDoList</div>
+                                <div className={style.portfolioItem_title}>React, Redux, TypeScript, MUI, Css-modules, Scss, React-dom, React-router-dom, Thunk.</div>
                                 <div className={style.portfolioItem_subtitle}><a className={style.code}
                                                                                  href="https://github.com/layout-IT/ToDolist">Code</a>
                                 </div>
@@ -48,7 +64,11 @@ export function Portfolios() {
                                    href={'https://layout-it.github.io/social_network/'} target={'_blank'}>
                                     <img src={SocialNetwork} alt="photo"/>
                                 </a>
-                                <div className={style.portfolioItem_title}>Social network</div>
+                                <div className={style.portfolioItem_title}>React, Redux, TypeScript, MUI,
+                                    React-dom, React-router-dom, React-redux, Css-modules, Scss, Thunk,
+                                    Redux-form, Axios.
+
+                                </div>
                                 <div className={style.portfolioItem_subtitle}><a className={style.code}
                                                                                  href="https://github.com/layout-IT/social_network">Code</a>
                                 </div>
@@ -61,9 +81,63 @@ export function Portfolios() {
                                    target={'_blank'}>
                                     <img src={Freelance} alt="photo"/>
                                 </a>
-                                <div className={style.portfolioItem_title}>My part of the freelance project</div>
+                                <div className={style.portfolioItem_title}>React, Redux, TypeScript, MUI,
+                                    React-dom, React-router-dom, React-redux, Css-modules, Scss, Thunk, Axios.</div>
                                 <div className={style.portfolioItem_subtitle}><a className={style.code}
                                                                                  href="https://github.com/layout-IT/Project">Code</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className={style.portfolio_colums}>
+                            <div className={style.portfolio_item}>
+                                <a className={style.portfolioItem_img} href={'https://layout-it.github.io/Gs_iron/'}
+                                   target={'_blank'}>
+                                    <img src={GSIron} alt="photo"/>
+                                </a>
+                                <div className={style.portfolioItem_title}>React, Redux, TypeScript, MUI,
+                                    React-dom, React-router-dom, React-redux, Css-modules, Scss, Thunk, Axios. ...in progress</div>
+                                <div className={style.portfolioItem_subtitle}><a className={style.code}
+                                                                                 href="https://github.com/layout-IT/Gs_iron">Code</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className={style.portfolio_colums}>
+                            <div className={style.portfolio_item}>
+                                <a className={style.portfolioItem_img} href={'https://layout-it.github.io/Calculator/'}
+                                   target={'_blank'}>
+                                    <img src={Calculator} alt="photo"/>
+                                </a>
+                                <div className={style.portfolioItem_title}>React, Redux, TypeScript, Css-modules, Scss, React-dom</div>
+                                <div className={style.portfolioItem_subtitle}><a className={style.code}
+                                                                                 href="https://github.com/layout-IT/Calculator">Code</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className={style.portfolio_colums}>
+                            <div className={style.portfolio_item}>
+                                <a className={style.portfolioItem_img} href={'https://layout-it.github.io/counter/'}
+                                   target={'_blank'}>
+                                    <img src={Counter} alt="photo"/>
+                                </a>
+                                <div className={style.portfolioItem_title}>React, Redux, TypeScript, Css-modules, Scss, React-redux, Localstorage</div>
+                                <div className={style.portfolioItem_subtitle}><a className={style.code}
+                                                                                 href="https://github.com/layout-IT/counter">Code</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className={style.portfolio_colums}>
+                            <div className={style.portfolio_item}>
+                                <a className={style.portfolioItem_img} href={'https://layout-it.github.io/Test_task_on_pure_js/'}
+                                   target={'_blank'}>
+                                    <img src={TableOnPureJs} alt="photo"/>
+                                </a>
+                                <div className={style.portfolioItem_title}>HTML, Css, Js</div>
+                                <div className={style.portfolioItem_subtitle}><a className={style.code}
+                                                                                 href="https://github.com/layout-IT/Test_task_on_pure_js">Code</a>
                                 </div>
 
                             </div>
@@ -74,7 +148,7 @@ export function Portfolios() {
                                    target={'_blank'}>
                                     <img src={Alpine} alt="photo"/>
                                 </a>
-                                <div className={style.portfolioItem_title}>Alpine</div>
+                                <div className={style.portfolioItem_title}>HTML, Scss, JS, Gulp.</div>
                                 <div className={style.portfolioItem_subtitle}><a className={style.code}
                                                                                  href="https://github.com/layout-IT/Alpine">Code</a>
                                 </div>
@@ -87,7 +161,7 @@ export function Portfolios() {
                                    target={'_blank'}>
                                     <img src={Nasscorp} alt="photo"/>
                                 </a>
-                                <div className={style.portfolioItem_title}>Nasscorp</div>
+                                <div className={style.portfolioItem_title}>HTML, Scss, JS, Gulp.</div>
                                 <div className={style.portfolioItem_subtitle}><a className={style.code}
                                                                                  href="https://github.com/layout-IT/Nasscorp">Code</a>
                                 </div>
@@ -100,7 +174,7 @@ export function Portfolios() {
                                    target={'_blank'}>
                                     <img src={Frispes} alt="photo"/>
                                 </a>
-                                <div className={style.portfolioItem_title}>Frispes</div>
+                                <div className={style.portfolioItem_title}>HTML, Scss, JS, Gulp.</div>
                                 <div className={style.portfolioItem_subtitle}><a className={style.code}
                                                                                  href="https://github.com/layout-IT/Frispies">Code</a>
                                 </div>
